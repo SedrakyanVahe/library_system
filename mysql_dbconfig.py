74574ffcf1239e93386d2947d94859c04cd6bf43
SedrakyanVahe <sedrakyanvahe5@gmail.com>
2022-01-19 09:58:33 +0400

    .

--- mysql_dbconfig.py
+++ mysql_dbconfig.py
@@ -7,9 +7,9 @@ try:
     db = cn.connect(
         host="localhost",
         port="3306",
-        user="vahe",
-        password="1234",
+        user="yourUserName",
+        password="yourPassword",
         database="library_system",
     )
 except:
-    print("errror")
+    print("error")
